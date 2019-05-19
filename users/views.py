f2dedb270653ff1de60fbc340cf27416c3a4b390
Stephen P <stephenparkhum@outlook.com>
2019-05-19 13:55:17 -0700

    Adding Register/Add-Artist Functionality
    ## Adding Register/Add-Artist Functionality

--- users/views.py
+++ users/views.py
@@ -4,20 +4,19 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(
-                request, f'Your account has been created, you are now able to login!')
+            messages.success(request, f'Your account has been created, you are now able to login!')
             return redirect('login')
 
-    else:
+    else: 
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
-
 
 @login_required
 def profile(request):
